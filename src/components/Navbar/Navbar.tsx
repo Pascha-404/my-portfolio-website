8ab8806abd8a0a26516b50bbcc40a2dc07f0abf4
Patrick Pavliuchik <73493957+Pascha-404@users.noreply.github.com>
2023-10-19 14:51:01 +0000

    add Image component with personal logo

--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -1,7 +1,21 @@
 import styles from './Navbar.module.scss';
+import Logo from '../../../public/personal-logo.svg';
+import Image from 'next/image';
 
 function Navbar(): JSX.Element {
-	return <nav></nav>;
+	return (
+		<nav className={styles.navbar}>
+			<div>
+				<Image
+					src={Logo}
+					width={200}
+					height={37}
+					alt='Patrick Pavliuchik typed in code syntax'
+				/>
+			</div>
+			<div></div>
+		</nav>
+	);
 }
 
 export default Navbar;
