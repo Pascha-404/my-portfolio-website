cb7632b4ce48cb14ddf5cf9ffd9ce304ba67cce5
Patrick Pavliuchik <73493957+Pascha-404@users.noreply.github.com>
2023-10-20 17:31:09 +0000

    add classes and heading

--- src/components/ProjectsSection/ProjectsSection.tsx
+++ src/components/ProjectsSection/ProjectsSection.tsx
@@ -1,7 +1,9 @@
 import styles from './ProjectsSection.module.scss';
 
 function ProjectsSection(): JSX.Element {
-	return <section className={styles.projectsSection}></section>;
+    return <section className={styles.projectsSection}>
+        <h3>My <span className={styles.colorHighlight}>projects</span></h3>
+    </section>;
 }
 
 export default ProjectsSection;
