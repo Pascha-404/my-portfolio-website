d14f1e238fc581f9fa0316a812a1a314651fb0f3
Patrick Pavliuchik <73493957+Pascha-404@users.noreply.github.com>
2023-10-21 11:36:28 +0000

    add interface for component props

--- src/components/ProjectCard/ProjectCard.tsx
+++ src/components/ProjectCard/ProjectCard.tsx
@@ -1,6 +1,18 @@
 import styles from './ProjectCard.module.scss';
 
-function ProjectCard(): JSX.Element {
+interface ProjectCardProps {
+	id: string;
+	imgSrc: string;
+	name: string;
+	role: string;
+	stack: string[];
+	tasks: string[];
+	description: string;
+	githubLink: string;
+	liveLink?: string;
+}
+
+function ProjectCard(props: ProjectCardProps): JSX.Element {
 	return <div className={styles.projectCard}></div>;
 }
 
