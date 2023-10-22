15b3123f710c4f8432c34dc58b33cf41baab6a04
Patrick Pavliuchik <73493957+Pascha-404@users.noreply.github.com>
2023-10-22 14:00:14 +0000

    add typescript directory, add types file and add TechType type

---
+++ src/ts/types.tsx
@@ -0,0 +1,14 @@
+type TechType =
+	| 'React'
+	| 'Next.js'
+	| 'Express.js'
+	| 'Node.js'
+	| 'JavaScript'
+	| 'SASS'
+	| 'HTML'
+	| 'CSS'
+	| 'AWS'
+	| 'MongoDB'
+	| 'Cloudinary';
+
+export type { TechType };
