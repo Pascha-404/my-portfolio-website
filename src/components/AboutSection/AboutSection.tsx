dde04574b558608075275f98b1744c2f7f5ee7c0
Patrick Pavliuchik <73493957+Pascha-404@users.noreply.github.com>
2023-10-24 14:09:36 +0000

    add starter code, add import/export for component directory

---
+++ src/components/AboutSection/AboutSection.tsx
@@ -0,0 +1,10 @@
+import styles from './AboutSection.module.scss';
+
+function AboutSection(): JSX.Element {
+	return (
+		<section className={styles.aboutSection}>
+		</section>
+	);
+}
+
+export default AboutSection;
