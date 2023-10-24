5e88f9550b69173ccf5471bd54a8602f489934b3
Patrick Pavliuchik <73493957+Pascha-404@users.noreply.github.com>
2023-10-24 20:56:41 +0000

    add html structure for component, add interface for props, populate list through data prop

--- src/components/DataListCard/DataListCard.tsx
+++ src/components/DataListCard/DataListCard.tsx
@@ -1,7 +1,54 @@
 import styles from './DataListCard.module.scss';
 
-function DataListCard(): JSX.Element {
-	return <div className={styles.dataListCard}></div>;
+interface DataListCardProps {
+	type: 'experience' | 'skill';
+	header: string;
+	role?: string;
+	timeRange?: string;
+	data: string[];
+}
+
+function DataListCard({
+	type,
+	header,
+	role,
+	timeRange,
+	data,
+}: DataListCardProps): JSX.Element {
+	switch (type) {
+		case 'experience':
+			return (
+				<div className={styles.dataListCard}>
+					<div>
+						<div>
+							<h6>{header}</h6>
+							<h6>{role}</h6>
+						</div>
+						<p>{timeRange}</p>
+					</div>
+
+					<ul className={styles.dataList}>
+						{data.map(item => (
+							<li className={styles.dataItem}>{item}</li>
+						))}
+					</ul>
+				</div>
+			);
+		case 'skill':
+			return (
+				<div className={styles.dataListCard}>
+					<h6>{header}</h6>
+
+					<ul className={styles.dataList}>
+						{data.map(item => (
+							<li className={styles.dataItem}>{item}</li>
+						))}
+					</ul>
+				</div>
+			);
+		default:
+			throw new Error(`Wrong type provided: ${type}`);
+	}
 }
 
 export default DataListCard;
