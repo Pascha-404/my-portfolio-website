daf544ff8075ef18c45de30bc09543198a89281e
Patrick Pavliuchik <73493957+Pascha-404@users.noreply.github.com>
2023-10-28 13:37:18 +0000

    add case/type for Project page list

--- src/components/DataListCard/DataListCard.tsx
+++ src/components/DataListCard/DataListCard.tsx
@@ -1,7 +1,7 @@
 import styles from './DataListCard.module.scss';
 
 interface DataListCardProps {
-	type: 'experience' | 'skill';
+	type: 'home_experience' | 'home_skill' | 'project_bulletlist';
 	header: string;
 	role?: string;
 	timeRange?: string;
@@ -16,7 +16,7 @@ function DataListCard({
 	data,
 }: DataListCardProps): JSX.Element {
 	switch (type) {
-		case 'experience':
+		case 'home_experience':
 			return (
 				<div className={styles.dataListCard}>
 					<div className={styles.cardHeaderWrapper}>
@@ -34,10 +34,22 @@ function DataListCard({
 					</ul>
 				</div>
 			);
-		case 'skill':
+		case 'home_skill':
 			return (
 				<div className={styles.dataListCard}>
 					<h6 className={styles.cardHeader}>{header}</h6>
+
+					<ul className={styles.cardList}>
+						{data.map(item => (
+							<li className={styles.cardListItem}>{item}</li>
+						))}
+					</ul>
+				</div>
+			);
+		case 'project_bulletlist':
+			return (
+				<div className={styles.dataListCard}>
+					<h2 className={styles.cardHeader}>{header}</h2>
 
 					<ul className={styles.cardList}>
 						{data.map(item => (
