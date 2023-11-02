7cb5d6f4a794f4070ea68a88063d86e7ba5904c1
Patrick Pavliuchik <73493957+Pascha-404@users.noreply.github.com>
2023-11-02 17:06:16 +0000

    add first version of code for component

---
+++ src/components/NavigationButton/NavigationButton.tsx
@@ -0,0 +1,59 @@
+import Link from 'next/link';
+import styles from './NavigationButton.module.scss';
+import React from 'react';
+
+interface NavigationButtonProps {
+	style: 'button' | 'underlined' | 'normal';
+	text: string;
+	targetId: string; // section where view should go to
+}
+
+function NavigationButton({ style, text, targetId }: NavigationButtonProps): JSX.Element {
+	function handleClick(e: React.MouseEvent<HTMLAnchorElement>) {
+		e.preventDefault();
+		const targetElement = document.querySelector(`#${targetId}`);
+
+		if (targetElement) {
+			targetElement.scrollIntoView({ behavior: 'smooth' });
+		} else {
+			console.log('Section not here')
+		}
+	}
+
+	switch (style) {
+		case 'normal':
+			return (
+				<Link
+					href='#'
+					onClick={handleClick}
+					className={`${styles.navigationButton} ${styles.normal}`}>
+					{text}
+				</Link>
+			);
+
+		case 'underlined':
+			return (
+				<Link
+					href='#'
+					onClick={handleClick}
+					className={`${styles.navigationButton} ${styles.underlined}`}>
+					{text}
+				</Link>
+			);
+
+		case 'button':
+			return (
+				<Link
+					href='#'
+					onClick={handleClick}
+					className={`${styles.navigationButton} ${styles.button}`}>
+					{text}
+				</Link>
+			);
+
+		default:
+			throw new Error(`No case found for provided style: ${style}`);
+	}
+}
+
+export default NavigationButton;
