152e979008d60a9755c6e2084dc08d21a67c02d8
Patrick Pavliuchik <73493957+Pascha-404@users.noreply.github.com>
2023-11-05 15:34:31 +0000

    add starter code

---
+++ src/components/ContactSection/ContactSection.tsx
@@ -0,0 +1,7 @@
+import styles from './ContactSection.module.scss';
+
+function ContactSection(): JSX.Element {
+	return <section className={styles.contactSection}></section>;
+}
+
+export default ContactSection;
