6d8ecdafdee4fdc9801b0e1c3ed20a3d7eee4daa
Patrick Pavliuchik <73493957+Pascha-404@users.noreply.github.com>
2023-11-05 16:38:22 +0000

    add div wrapper for calendly component

--- src/components/ContactSection/ContactSection.tsx
+++ src/components/ContactSection/ContactSection.tsx
@@ -1,7 +1,11 @@
 import styles from './ContactSection.module.scss';
 
 function ContactSection(): JSX.Element {
-	return <section className={styles.contactSection}></section>;
+	return (
+		<section className={styles.contactSection}>
+			<div className={styles.calendlyWrapper}></div>
+		</section>
+	);
 }
 
 export default ContactSection;
