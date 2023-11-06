d1f81ecefb81ce465bd890a588abaca1dd3f8c86
Patrick Pavliuchik <73493957+Pascha-404@users.noreply.github.com>
2023-11-06 18:11:05 +0000

    replace NavigationButton with CalendlyButton component for all booking buttons

--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -1,16 +1,19 @@
-import Image from 'next/image';
+import dynamic from 'next/dynamic';
 import Logo from '../../../public/personal-logo.svg';
 
+import Image from 'next/image';
 import LanguageButton from '../LanguageButton';
 import NavigationButton from '../NavigationButton';
 
 import styles from './Navbar.module.scss';
 
+const CalendlyButton = dynamic(() => import('../CalendlyButton'), { ssr: false });
+
 function Navbar(): JSX.Element {
 	return (
 		<nav id='navbar' className={styles.navbar}>
 			<div>
-				<a href="/">
+				<a href='/'>
 					<Image
 						src={Logo}
 						width={200}
@@ -25,7 +28,7 @@ function Navbar(): JSX.Element {
 				<NavigationButton text='about' style='normal' targetId='aboutSection' />
 				<NavigationButton text='projects' style='normal' targetId='projectsSection' />
 				<NavigationButton text='contact' style='normal' targetId='contactSection' />
-				<NavigationButton text="Let's talk?" style='button' targetId='contactSection'/>
+				<CalendlyButton text="Let's talk?" />
 			</div>
 		</nav>
 	);
