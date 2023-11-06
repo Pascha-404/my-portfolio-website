41f9c4c1143447a4944be4cae0c091050cc4eca4
Patrick Pavliuchik <73493957+Pascha-404@users.noreply.github.com>
2023-11-06 18:17:46 +0000

    move html/styling for "Lets chat?" section into ContactSection

--- src/components/ContactSection/ContactSection.tsx
+++ src/components/ContactSection/ContactSection.tsx
@@ -1,15 +1,22 @@
-import dynamic from 'next/dynamic';
+import CalendlyButton from '../CalendlyButton';
 
 import styles from './ContactSection.module.scss';
-
-// Lazy loads the component
-const CalendlyButton = dynamic(() => import('../CalendlyButton'), { ssr: false });
 
 function ContactSection(): JSX.Element {
 	return (
 		<section id='contactSection' className={styles.contactSection}>
+			<h5>
+				Let's <span className={styles.colorHighlight}>chat?</span>
+			</h5>
+
+			<p>
+				Whether you have a project in mind, want to discuss potential collaborations, or
+				simply want to chat about design and technology, I'm here for it. Feel free to
+				reach out by filling out the form below to book a call with me.
+			</p>
+
 			<div className={styles.calendlyWrapper}>
-				<CalendlyButton text='Book a call'/>
+				<CalendlyButton text='Book a call' />
 			</div>
 		</section>
 	);
