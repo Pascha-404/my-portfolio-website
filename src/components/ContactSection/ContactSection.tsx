463db35030ae274d528d7eb3cd480084ffd49d10
Patrick Pavliuchik <73493957+Pascha-404@users.noreply.github.com>
2023-11-06 16:58:53 +0000

    add lazy load for Calendly Component, add Calendly Component

--- src/components/ContactSection/ContactSection.tsx
+++ src/components/ContactSection/ContactSection.tsx
@@ -1,9 +1,16 @@
+import dynamic from 'next/dynamic';
+
 import styles from './ContactSection.module.scss';
+
+// Lazy loads the component
+const CalendlyButton = dynamic(() => import('../CalendlyButton'), { ssr: false });
 
 function ContactSection(): JSX.Element {
 	return (
-		<section className={styles.contactSection}>
-			<div className={styles.calendlyWrapper}></div>
+		<section id='contactSection' className={styles.contactSection}>
+			<div className={styles.calendlyWrapper}>
+				<CalendlyButton text='Book a call'/>
+			</div>
 		</section>
 	);
 }
