835bb3a454620e44dee014fbe5ad28914b844326
Patrick Pavliuchik <73493957+Pascha-404@users.noreply.github.com>
2023-11-18 16:42:42 +0000

    add interface types for content that has different translations, add basic interface for StaticContent

--- src/ts/types.tsx
+++ src/ts/types.tsx
@@ -11,4 +11,23 @@ type TechType =
 	| 'MongoDB'
 	| 'Cloudinary';
 
+interface ITranslations {
+	en: string;
+	de: string;
+}
+
+interface IHighlight extends ITranslations {
+	highlight: ITranslations;
+}
+
+interface IStaticContent {
+	section_name: string;
+	description?: ITranslations;
+	header?: ITranslations | IHighlight;
+	sub_header?: ITranslations;
+	action_btn?: ITranslations;
+	navigation_btn?: ITranslations;
+	logo_alt?: ITranslations;
+}
+
 export type { TechType };
