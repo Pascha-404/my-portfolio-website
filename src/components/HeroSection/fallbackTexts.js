e8d9cfb3a4be0c214bbc3f13e1f73e85562f4b76
Patrick Pavliuchik <73493957+Pascha-404@users.noreply.github.com>
2023-11-24 22:20:30 +0000

    add file for fallback content

---
+++ src/components/HeroSection/fallbackTexts.js
@@ -0,0 +1,14 @@
+const fallbackTexts = {
+	header: "Hi I'm",
+	sub_header: 'software engineer, who loves CHALLENGES and QUIZES.',
+	description:
+		'Are you looking for a passionate full stack developer with a love for creating meaningful digital experiences? Explore my portfolio to discover my journey through various projects and technologies.',
+	fit: 'Maybe we could make a good fit?',
+	navigation_btn: 'more about me, my experience and portfolio',
+	action_btn: 'book a call',
+	image_alt: 'Front photo of Patrick Pavliuchik',
+	arrow_alt: 'Long arrow icon pointing downwards',
+};
+
+
+export default fallbackTexts;
