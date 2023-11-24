902ebeecd03c04b18bc7556af8aeb31360dce16b
Patrick Pavliuchik <73493957+Pascha-404@users.noreply.github.com>
2023-11-24 22:19:43 +0000

    add "use client", add variables for content from staticContent data, add fallback cases

--- src/components/HeroSection/HeroSection.tsx
+++ src/components/HeroSection/HeroSection.tsx
@@ -1,6 +1,9 @@
+'use client';
+
 import HeroImg from '../../../public/patrick-hero-img.png';
 import { iconArrow } from '../../../public/icons';
 
+import fallbackTexts from './fallbackTexts';
 import { IHeroSectionContent } from '@/ts/types';
 
 import Image from 'next/image';
@@ -8,46 +11,58 @@ import NavigationButton from '../NavigationButton';
 import CalendlyButton from '../CalendlyButton';
 
 import styles from './HeroSection.module.scss';
+import { useLanguage } from '@/utils/LanguageContext';
 
 function HeroSection({
 	staticContent,
 }: {
 	staticContent: IHeroSectionContent;
 }): JSX.Element {
+	const { currentLanguage } = useLanguage();
+	const headerText = staticContent.header?.[currentLanguage] || fallbackTexts.header;
+	const subHeaderText =
+		staticContent.sub_header?.[currentLanguage] || fallbackTexts.sub_header;
+	const descriptionText =
+		staticContent.description?.[currentLanguage] || fallbackTexts.description;
+	const fitText = staticContent.fit[currentLanguage] || fallbackTexts.fit;
+	const navigationBtnText =
+		staticContent.navigation_btn?.[currentLanguage] || fallbackTexts.navigation_btn;
+	const actionBtnText =
+		staticContent.action_btn?.[currentLanguage] || fallbackTexts.action_btn;
+	const imageAltText =
+		staticContent.image_alt[currentLanguage] || fallbackTexts.image_alt;
+	const arrowAltText =
+		staticContent.arrow_alt[currentLanguage] || fallbackTexts.arrow_alt;
+	
 	return (
 		<section className={styles.heroSection}>
 			<div className={styles.heroFirstHalf}>
 				<h1>
-					Hi, I'm <span className={styles.colorHighlight}>Patrick</span>
+					{headerText} <span className={styles.colorHighlight}>Patrick</span>
 				</h1>
 
 				<h2>
-					software engineer, who loves{' '}
-					<span className={styles.sizeHighlight}>challenges</span> and{' '}
+					{subHeaderText}
 					<span className={styles.sizeHighlight}>quizes.</span>
 				</h2>
 
-				<p>
-					Are you looking for a passionate full stack developer with a love for creating
-					meaningful digital experiences? Explore my portfolio to discover my journey
-					through various projects and technologies.
-				</p>
+				<p>{descriptionText}</p>
 
-				<p>Maybe we could make a good fit?</p>
+				<p>{fitText}</p>
 
 				<div className={styles.links}>
 					<NavigationButton
-						text='more about me, my experience and portfolio'
+						text={navigationBtnText}
 						style='underlined'
 						targetId='projectsSection'
 					/>
-					<CalendlyButton text='book a call' />
+					<CalendlyButton text={actionBtnText} />
 				</div>
 
-				<Image src={iconArrow} alt='Long arrow icon pointing downwards' />
+				<Image src={iconArrow} alt={arrowAltText} />
 			</div>
 			<div className={styles.heroSecondHalf}>
-				<Image src={HeroImg} alt='Front photo of Patrick Pavliuchik' priority />
+				<Image src={HeroImg} alt={imageAltText} priority />
 			</div>
 		</section>
 	);
