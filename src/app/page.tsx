eab92922ab7a65e435a4c8108ed5d1dd7e435a04
Patrick Pavliuchik <73493957+Pascha-404@users.noreply.github.com>
2023-11-24 21:16:36 +0000

    filter staticContent for each section and pass down to components

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,17 +6,29 @@ import AboutSection from '@/components/AboutSection';
 import ContactSection from '@/components/ContactSection';
 
 import styles from './page.module.scss';
+import { IHeroSectionContent, IStaticContent, IAboutSectionContent } from '@/ts/types';
 
-	
 async function Home() {
-	const staticContent = await getStaticContent()
+	const staticContent = await getStaticContent();
+	const heroContent: IHeroSectionContent = staticContent.find(
+		(obj: IHeroSectionContent) => obj.section_name === 'heroSection'
+	);
+	const projectsContent: IStaticContent = staticContent.find(
+		(obj: IStaticContent) => obj.section_name === 'projectsSection'
+	);
+	const aboutContent: IAboutSectionContent = staticContent.find(
+		(obj: IAboutSectionContent) => obj.section_name === 'aboutSection'
+	);
+	const contactContent: IStaticContent = staticContent.find(
+		(obj: IStaticContent) => obj.section_name === 'contactSection'
+	);
 
 	return (
 		<main className={styles.main}>
-			<HeroSection />
-			<ProjectsSection />
-			<AboutSection />
-			<ContactSection />
+			<HeroSection staticContent={heroContent} />
+			<ProjectsSection staticContent={projectsContent} />
+			<AboutSection staticContent={aboutContent} />
+			<ContactSection staticContent={contactContent} />
 		</main>
 	);
 }
