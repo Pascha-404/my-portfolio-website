3bd833e8ed92a602346392d14c36fcce066be584
Patrick Pavliuchik <73493957+Pascha-404@users.noreply.github.com>
2023-11-24 17:07:08 +0000

    fetch staticContent into component, formate code

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,16 @@
+import { getStaticContent } from '@/utils';
+
 import HeroSection from '@/components/HeroSection';
 import ProjectsSection from '@/components/ProjectsSection';
-
-import styles from './page.module.scss';
 import AboutSection from '@/components/AboutSection';
 import ContactSection from '@/components/ContactSection';
 
-export default function Home() {
+import styles from './page.module.scss';
+
+	
+async function Home() {
+	const staticContent = await getStaticContent()
+
 	return (
 		<main className={styles.main}>
 			<HeroSection />
@@ -15,3 +20,5 @@ export default function Home() {
 		</main>
 	);
 }
+
+export default Home;
