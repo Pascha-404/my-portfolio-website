f8555b320060922ea571584b208178eb40ce2f12
Patrick Pavliuchik <73493957+Pascha-404@users.noreply.github.com>
2023-11-24 16:33:01 +0000

    grab content data from staticContent prop, add Data to components inside of Navbar, add default cases for content

--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -1,5 +1,6 @@
 import dynamic from 'next/dynamic';
 import Logo from '../../../public/personal-logo.svg';
+import { INavbarContent } from '@/ts/types';
 
 import Image from 'next/image';
 import LanguageButton from '../LanguageButton';
@@ -9,26 +10,28 @@ import styles from './Navbar.module.scss';
 
 const CalendlyButton = dynamic(() => import('../CalendlyButton'), { ssr: false });
 
-function Navbar(): JSX.Element {
+function Navbar({ staticContent }: { staticContent: INavbarContent }): JSX.Element {
+	const currentLanguage = 'en';
+	const aboutText = staticContent.about[currentLanguage] || 'about';
+	const projectsText = staticContent.projects[currentLanguage] || 'projects';
+	const contactText = staticContent.contact[currentLanguage] || 'contact';
+	const actionBtnText = staticContent.action_btn?.[currentLanguage] || "Let's Talk?";
+	const logoAltText = staticContent.logo_alt?.[currentLanguage] || 'Logo Alt';
+
 	return (
 		<nav id='navbar' className={styles.navbar}>
 			<div>
 				<a href='/'>
-					<Image
-						src={Logo}
-						width={200}
-						height={37}
-						alt='Patrick Pavliuchik typed in code syntax'
-						priority
-					/>
+					<Image src={Logo} width={200} height={37} alt={logoAltText} priority />
 				</a>
 			</div>
+
 			<div className={styles.navbarLinks}>
 				<LanguageButton currentLanguage='eng' />
-				<NavigationButton text='about' style='normal' targetId='aboutSection' />
-				<NavigationButton text='projects' style='normal' targetId='projectsSection' />
-				<NavigationButton text='contact' style='normal' targetId='contactSection' />
-				<CalendlyButton text="Let's talk?" />
+				<NavigationButton text={aboutText} style='normal' targetId='aboutSection' />
+				<NavigationButton text={projectsText} style='normal' targetId='projectsSection' />
+				<NavigationButton text={contactText} style='normal' targetId='contactSection' />
+				<CalendlyButton text={actionBtnText} />
 			</div>
 		</nav>
 	);
