3c82dcb67578a7ff4d362cb80506002363167f50
Patrick Pavliuchik <73493957+Pascha-404@users.noreply.github.com>
2023-12-03 18:48:02 +0000

    add "use client", utilise LanguageContext, create variables for textContent and add them to component return

--- src/components/ProjectsSection/ProjectsSection.tsx
+++ src/components/ProjectsSection/ProjectsSection.tsx
@@ -1,22 +1,38 @@
+'use client';
+
 import { iconArrow } from '../../../public/icons';
 
 import Image from 'next/image';
 import ProjectCard from '../ProjectCard';
 
 import { IStaticContent } from '@/ts/types';
+import fallbackContent from './fallbackContent';
+import textMapFunction from '@/utils/textMapFunction';
 
 import styles from './ProjectsSection.module.scss';
+import { useLanguage } from '@/utils/LanguageContext';
 
 function ProjectsSection({
 	staticContent,
 }: {
 	staticContent: IStaticContent;
 }): JSX.Element {
+	const { currentLanguage } = useLanguage();
+	const headerText = textMapFunction({
+		objectArray: staticContent.header || fallbackContent.header,
+		currentLanguage,
+		stylingClass: 'colorHighlight',
+		styles,
+	});
+	const navButtonText =
+		staticContent.navigation_btn?.[currentLanguage] ||
+		fallbackContent.navigation_btn[currentLanguage];
+	const arrowAltText =
+		staticContent.arrow_alt?.[currentLanguage] ||
+		fallbackContent.arrow_alt[currentLanguage];
 	return (
 		<section id='projectsSection' className={styles.projectsSection}>
-			<h3>
-				My <span className={styles.colorHighlight}>projects</span>
-			</h3>
+			<h3>{headerText}</h3>
 			<ProjectCard
 				id='1'
 				name='Test-Project'
@@ -49,11 +65,11 @@ function ProjectsSection({
 			/>
 
 			<a className={styles.aboutLink} href='/'>
-				more about me and my experience
+				{navButtonText}
 			</a>
 
 			<div className={styles.imgWrapper}>
-				<Image src={iconArrow} alt='Yellow arrow pointing downwards' />
+				<Image src={iconArrow} alt={arrowAltText} />
 			</div>
 		</section>
 	);
