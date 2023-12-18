8bcc0a2f9cc987950005726c456bae248eb3d893
Patrick Pavliuchik <73493957+Pascha-404@users.noreply.github.com>
2023-12-18 19:17:55 +0700

    add handler for providing content from local file if db is not reachable

--- src/utils/getStaticContent.tsx
+++ src/utils/getStaticContent.tsx
@@ -1,18 +1,23 @@
 import { cache } from 'react';
 import { connectToDatabase } from '.';
+import { static_sections } from './fallbackContent';
 
 const revalidate: number = 3600;
 
 const getStaticContent = cache(async () => {
-    const connection = await connectToDatabase();
-    
-    if (!connection) {
-        throw new Error('Failed to connect to the database!')
-    }
-    const { db } = connection;
-	const fetchedData = await db.collection('static_content').find({}).toArray();
-	const transformedData = JSON.parse(JSON.stringify(fetchedData));
-	return transformedData[0].static_sections;
+	const connection = await connectToDatabase();
+
+	if (connection) {
+		const { db } = connection;
+		const fetchedData = await db.collection('static_content').find({}).toArray();
+		const transformedData = JSON.parse(JSON.stringify(fetchedData));
+
+		return transformedData[0].static_sections;
+	}
+
+	console.error('application uses fallback content');
+
+	return static_sections;
 });
 
 export { revalidate, getStaticContent };
