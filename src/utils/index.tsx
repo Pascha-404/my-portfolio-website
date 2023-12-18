7eae0c05c9cd7dcaf8ffdbf7421f447d9af51ee8
Patrick Pavliuchik <73493957+Pascha-404@users.noreply.github.com>
2023-12-18 19:19:58 +0700

    remove textMapFunction from file

--- src/utils/index.tsx
+++ src/utils/index.tsx
@@ -1,5 +1,4 @@
 import connectToDatabase from './dbConnect';
 import { getStaticContent } from './getStaticContent';
-import textMapFunction from './textMapFunction';
 
-export { connectToDatabase, getStaticContent, textMapFunction };
+export { connectToDatabase, getStaticContent };
