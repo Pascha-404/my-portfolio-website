7b31e2774d9d9ff115d7bfe229f78797660ee4cc
Patrick Pavliuchik <73493957+Pascha-404@users.noreply.github.com>
2023-12-20 19:11:32 +0700

    feat: integrate db data through staticContent prop in Footer
    Spreaded data coming from staticContent prop (db || fallbackContent data) in elements of component. I made the component a client side component to utilise the Language context and display content dynamically based on the language choice of the user.

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -1,27 +1,42 @@
+'use client';
+
 import { iconArrowUp } from '../../../public/icons';
 import personalLogo from '../../../public/personal-logo.svg';
 
 import Image from 'next/image';
 import NavigationButton from '../NavigationButton';
+import { IFooterContent } from '@/ts/types';
+import { useLanguage } from '@/utils/client';
 
 import styles from './Footer.module.scss';
 
-function Footer(): JSX.Element {
+function Footer({ staticContent }: { staticContent: IFooterContent }): JSX.Element {
+	const { currentLanguage } = useLanguage();
+	const navBtnText = staticContent.navigation_btn![currentLanguage];
+	const arrowAltText = staticContent.arrow_alt![currentLanguage];
+	const contactsText = staticContent.contacts[currentLanguage];
+	const projectsText = staticContent.sitemap.projects[currentLanguage];
+	const aboutText = staticContent.sitemap.about[currentLanguage];
+	const resumeText = staticContent.sitemap.resume[currentLanguage];
+	const privacyPolicyText = staticContent.privacy[currentLanguage];
+	const imprintText = staticContent.imprint[currentLanguage];
+	const logoAltText = staticContent.logo_alt![currentLanguage];
+
 	return (
 		<footer className={styles.footer}>
 			<div className={styles.upLinkWrapper}>
 				<NavigationButton
-					text='up to the beginning'
+					text={navBtnText}
 					style='underlined'
 					targetId='navbar'
 				/>
 			</div>
 
 			<div className={styles.middleContent}>
-				<Image src={iconArrowUp} alt='Yellow arrow pointing upwards' />
+				<Image src={iconArrowUp} alt={arrowAltText} />
 
 				<div className={styles.contactWrapper}>
-					<h6>contacts</h6>
+					<h6>{contactsText}</h6>
 					<div>
 						<p>Patrick Pavliuchik,</p>
 						<a href='tel:04917782934'>+49 17782934</a>
@@ -32,24 +47,32 @@ function Footer(): JSX.Element {
 				<div className={styles.sitemap}>
 					<h6>sitemap</h6>
 					<div>
-						<NavigationButton text='projects' targetId='projectsSection' style='normalSmall'/>
-						<NavigationButton text='about me' targetId='aboutSection' style='normalSmall'/>
-						<NavigationButton text='resume' targetId='resume' style='normalSmall'/>
+						<NavigationButton
+							text={projectsText}
+							targetId='projectsSection'
+							style='normalSmall'
+						/>
+						<NavigationButton
+							text={aboutText}
+							targetId='aboutSection'
+							style='normalSmall'
+						/>
+						<NavigationButton text={resumeText} targetId='resume' style='normalSmall' />
 					</div>
 				</div>
 			</div>
 
 			<div className={styles.bottomContent}>
 				<div className={styles.logoWrapper}>
-					<a href="/">
-						<Image src={personalLogo} alt='Patrick Pavliuchik typed in code syntax' />
+					<a href='/'>
+						<Image src={personalLogo} alt={logoAltText} />
 					</a>
 					<p>&copy;</p>
 				</div>
 
 				<div className={styles.legal}>
-					<a href='/'>Impressum</a>
-					<a href='/'>Privacy Policy</a>
+					<a href='/'>{imprintText}</a>
+					<a href='/'>{privacyPolicyText}</a>
 				</div>
 			</div>
 		</footer>
