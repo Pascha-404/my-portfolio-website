49475c9e175e306a3d7e28559c1f5cfea9f57a4d
Patrick Pavliuchik <73493957+Pascha-404@users.noreply.github.com>
2024-01-22 17:56:11 +0000

    feat: Replace hard coded data with data from projectData prop
    Hard coded dummy data was replaced with the data fetched from the database.

--- src/components/ProjectPage/ProjectPage.tsx
+++ src/components/ProjectPage/ProjectPage.tsx
@@ -13,113 +13,62 @@ interface ProjectPageProps {
 
 function ProjectPage({ projectData }: ProjectPageProps): JSX.Element {
 	const { currentLanguage } = useLanguage();
+	const { name, imgSrc, githubLink, liveLink, yearCreated } = projectData;
+	const { role, longDescription, challenges, approach, results, technology } =
+		projectData[currentLanguage];
+	
 	return (
 		<main className={styles.main}>
 			<h1>
 				<span className={styles.uppercase}>project:</span>{' '}
-				<span className={styles.colorHighlight}>xy</span>
+				<span className={styles.colorHighlight}>{name}</span>
 			</h1>
 			<div className={styles.roleDateWrapper}>
-				<p>role</p>
-				<p>date</p>
+				<p>{role}</p>
+				<p>{yearCreated}</p>
 			</div>
 
-			<Image
-				src={defaultImage}
-				alt='Default image showing two phones'
-				priority
-				className={styles.projectImage}
-			/>
+			{imgSrc ? (
+				<Image
+					src={imgSrc}
+					alt='Project Screenshot'
+					priority
+					className={styles.projectImage}
+				/>
+			) : (
+				<Image
+					src={defaultImage}
+					alt='Default image showing two phones'
+					priority
+					className={styles.projectImage}
+				/>
+			)}
 
 			<div className={styles.overviewWrapper}>
 				<h2 className={styles.uppercase}>project overview</h2>
-				<p>
-					I undertook the development of a fullstack web application for a client in the
-					healthcare industry. The goal was to create a robust and user-friendly platform
-					that allowed healthcare providers to manage patient data securely and
-					efficiently. This case study outlines the key aspects of this project.
-				</p>
+				<p>{longDescription}</p>
 			</div>
 
 			<div className={styles.challengesWrapper}>
-				<DataListCard
-					type='project_bulletList'
-					header='challenges'
-					data={[
-						{
-							data: 'Security and Compliance: Healthcare data must be handled with utmost care due to privacy regulations. Ensuring data security and compliance with HIPAA standards was a primary challenge.',
-						},
-						{
-							data: 'Complex Data Structure: The application needed to handle a complex database structure that included patient records, medical history, appointment scheduling, and billing information.',
-						},
-						{
-							data: 'User-Friendly Interface: It was crucial to design an intuitive user interface for healthcare professionals who may not be tech-savvy, ensuring easy navigation and data input.',
-						},
-					]}
-				/>
+				<DataListCard type='project_bulletList' header='challenges' data={challenges} />
 			</div>
 
 			<div className={styles.approachWrapper}>
-				<DataListCard
-					type='project_highlightList'
-					header='approach'
-					data={[
-						{
-							header: 'Technology Stack',
-							data: 'I chose a technology stack that included React for the frontend and Node.js for the backend, ensuring a modern and scalable architecture.',
-						},
-						{
-							header: 'User Authentication',
-							data: 'Implemented secure user authentication with JWT tokens to protect patient data and meet HIPAA compliance requirements.',
-						},
-						{
-							header: 'Database Design',
-							data: 'Designed a relational database schema to efficiently store and retrieve patient information, while maintaining data integrity.',
-						},
-						{
-							header: 'Frontend Development',
-							data: 'Created a responsive and user-friendly frontend using React, incorporating features like patient record management, appointment scheduling, and billing.',
-						},
-						{
-							header: 'Backend Development',
-							data: 'Developed the backend using Node.js and Express, implementing RESTful APIs for data retrieval and manipulation.',
-						},
-						{
-							header: 'Testing and Quality Assurance',
-							data: "Conducted thorough testing, including unit tests and end-to-end testing, to ensure the application's reliability and security.",
-						},
-					]}
-				/>
+				<DataListCard type='project_highlightList' header='approach' data={approach} />
 			</div>
 
 			<div className={styles.resultsWrapper}>
-				<DataListCard
-					type='project_bulletList'
-					header='results'
-					data={[
-						{
-							data: 'Security Compliance: The application successfully met HIPAA compliance standards, ensuring patient data security.',
-						},
-						{
-							data: 'Efficient Data Management: Healthcare providers reported a significant improvement in managing patient data, resulting in time and cost savings.',
-						},
-						{
-							data: 'Positive User Feedback: Users praised the user-friendly interface, which led to higher user adoption rates.',
-						},
-					]}
-				/>
+				<DataListCard type='project_bulletList' header='results' data={results} />
 			</div>
 
 			<div className={styles.technologysWrapper}>
 				<h2 className={styles.uppercase}>technologies used</h2>
 				<div>
-					<p>Frontend: React, Redux</p>
-					<p>Backend: Node.js, Express</p>
-					<p>Database: PostgreSQL</p>
-					<p>User Authentication: JWT</p>
-					<p>RESTful API Design</p>
-					<p>Testing: Jest, Supertest</p>
-					<p>Deployment: Docker, AWS</p>
+					{technology.map((kind, idx) => (
+						<p key={`project-technology-${idx}`}>
+							{kind.category}: {kind.detail.join(', ')}
+						</p>
+					))}
 				</div>
 			</div>
 		</main>
