04557515e4531e3f44e41e97210ecd297e203ae8
Patrick Pavliuchik <73493957+Pascha-404@users.noreply.github.com>
2024-02-08 19:26:19 +0000

    refactor: extract content of error.tsx into a reusable ErrorSection component
    To reduce code duplication and enhance maintainability, the content previously within the return statement of error.tsx has been encapsulated into a separate ErrorSection component. This enables reuse across various error.tsx files.

--- src/app/project/[id]/error.tsx
+++ src/app/project/[id]/error.tsx
@@ -1,10 +1,6 @@
 'use client'; // Error components must be Client Components
 
-import { useLanguage } from '@/utils/client';
-import Image from 'next/image';
-
-import styles from './error.module.scss';
-import { iconFolder } from '#/icons';
+import ErrorSection from '@/components/ErrorSection';
 
 function Error({
 	error,
@@ -13,25 +9,7 @@ function Error({
 	error: Error & { digest?: string };
 	reset: () => void;
 }) {
-	const { currentLanguage } = useLanguage();
-
-	return (
-		<section className={styles.error}>
-			<Image src={iconFolder} priority alt='Error Icon'/>
-			<h2>
-				{currentLanguage === 'en'
-					? 'Something went wrong...'
-					: 'Irgendwas stimmt nicht...'}
-			</h2>
-			<button
-				onClick={
-					// Attempt to recover by trying to re-render the segment
-					() => reset()
-				}>
-				{currentLanguage === 'en' ? 'Try again' : 'Nochmal versuchen'}
-			</button>
-		</section>
-	);
+	return <ErrorSection reset={reset} />;
 }
 
 export default Error;
