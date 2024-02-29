5598a747b324a4585df61ec0d4065eb5543aefcc
Patrick Pavliuchik <73493957+Pascha-404@users.noreply.github.com>
2024-02-29 17:56:03 +0000

    feat: implement content and layout for imprint page
    Populated the imprint page with necessary content and established its layout structure.

--- src/components/ImprintPage/ImprintPage.tsx
+++ src/components/ImprintPage/ImprintPage.tsx
@@ -3,9 +3,22 @@ import styles from './ImprintPage.module.scss';
 function ImprintPage() {
 	return (
 		<main className={styles.main}>
-			<section className={styles.imprintPage}>
-				<h1>PAGE HEADER</h1>
-			</section>
+			<h1>Impressum</h1>
+
+			<p>
+				Patrick Pavliuchik
+				<br />
+				K&ouml;rtingstr. 61c
+				<br />
+				12107 Berlin
+			</p>
+
+			<h2>Kontakt</h2>
+			<p>
+				Telefon: +49 (0) 17794184354
+				<br />
+				E-Mail: patrick@pavliuchik.com
+			</p>
 		</main>
 	);
 }
