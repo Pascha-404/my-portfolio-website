a2ff2ae9b54c52d56512d8346ce5b733f04892b5
Patrick Pavliuchik <73493957+Pascha-404@users.noreply.github.com>
2024-03-01 17:36:00 +0000

    feat: implement LanguageContext for dynamic content
    Enhanced the component to dynamically display content in German and English, ensuring consistent availability of multilingual content based on LanguageContext.

--- src/components/ImprintPage/ImprintPage.tsx
+++ src/components/ImprintPage/ImprintPage.tsx
@@ -1,9 +1,13 @@
+'use client';
+
+import { useLanguage } from '@/utils/client';
 import styles from './ImprintPage.module.scss';
 
 function ImprintPage() {
+	const { currentLanguage } = useLanguage();
 	return (
 		<main className={styles.main}>
-			<h1>Impressum</h1>
+			<h1>{currentLanguage === 'de' ? 'Impressum' : 'Imprint'}</h1>
 
 			<p>
 				Patrick Pavliuchik
@@ -13,9 +17,9 @@ function ImprintPage() {
 				12107 Berlin
 			</p>
 
-			<h2>Kontakt</h2>
+			<h2>{currentLanguage === 'de' ? 'Kontakt' : 'Contact'}</h2>
 			<p>
-				Telefon: +49 (0) 17794184354
+				{currentLanguage === 'de' ? 'Telefon' : 'Phone'}: +49 (0) 17794184354
 				<br />
 				E-Mail: patrick@pavliuchik.com
 			</p>
