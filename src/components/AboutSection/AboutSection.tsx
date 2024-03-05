eb0e8e1074a1657c2467710225990d35d923b01d
Patrick Pavliuchik <73493957+Pascha-404@users.noreply.github.com>
2024-03-05 10:17:46 +0000

    Enhance UI with SectionTransition component
    Sections are now wrapped with the SectionTransition component, improving the visual experience of the application.

--- src/components/AboutSection/AboutSection.tsx
+++ src/components/AboutSection/AboutSection.tsx
@@ -2,6 +2,7 @@
 
 import { v4 as uuid } from 'uuid';
 
+import SectionTransition from '../SectionTransition';
 import DataListCard from '../DataListCard';
 import DownloadButton from '../DownloadButton';
 
@@ -39,54 +40,49 @@ function AboutSection({
 	const downloadButtonText = staticContent.experience.pdfLink[currentLanguage];
 
 	return (
-		<section id='aboutSection' className={styles.aboutSection}>
-			<div className={styles.aboutMe}>
-				<h5>{headerText}</h5>
-
-				<p>{descriptionText}</p>
-
-				<div className={styles.dataWrapper}>
-					<p>{skillsHeaderText}:</p>
-
-					<div className={styles.dataCardWrapper}>
-						<DataListCard
-							type='home_skill'
-							header='frontend'
-							data={staticContent.skills.frontend[currentLanguage]}
-						/>
-						
-						<DataListCard
-							type='home_skill'
-							header='backend'
-							data={staticContent.skills.backend[currentLanguage]}
-						/>
+		<SectionTransition>
+			<section id='aboutSection' className={styles.aboutSection}>
+				<div className={styles.aboutMe}>
+					<h5>{headerText}</h5>
+					<p>{descriptionText}</p>
+					<div className={styles.dataWrapper}>
+						<p>{skillsHeaderText}:</p>
+						<div className={styles.dataCardWrapper}>
+							<DataListCard
+								type='home_skill'
+								header='frontend'
+								data={staticContent.skills.frontend[currentLanguage]}
+							/>
+							<DataListCard
+								type='home_skill'
+								header='backend'
+								data={staticContent.skills.backend[currentLanguage]}
+							/>
+						</div>
 					</div>
 				</div>
-			</div>
-
-			<div className={styles.myExperience}>
-				<h5>{experienceHeaderText}</h5>
-
-				<div className={styles.dataWrapper}>
-					<DownloadButton text={`[${downloadButtonText}]`} id='resume' />
-
-					<div className={styles.dataCardWrapper}>
-						{staticContent.experience.companys[currentLanguage].map(company => {
-							return (
-								<DataListCard
-									key={`AboutSection-Company-${uuid()}`}
-									type='home_experience'
-									header={company.company_name}
-									role={company.company_role}
-									timeRange={company.time}
-									data={company.skills}
-								/>
-							);
-						})}
+				<div className={styles.myExperience}>
+					<h5>{experienceHeaderText}</h5>
+					<div className={styles.dataWrapper}>
+						<DownloadButton text={`[${downloadButtonText}]`} id='resume' />
+						<div className={styles.dataCardWrapper}>
+							{staticContent.experience.companys[currentLanguage].map(company => {
+								return (
+									<DataListCard
+										key={`AboutSection-Company-${uuid()}`}
+										type='home_experience'
+										header={company.company_name}
+										role={company.company_role}
+										timeRange={company.time}
+										data={company.skills}
+									/>
+								);
+							})}
+						</div>
 					</div>
 				</div>
-			</div>
-		</section>
+			</section>
+		</SectionTransition>
 	);
 }
 
--- src/components/ProjectsSection/ProjectsSection.tsx
+++ src/components/ProjectsSection/ProjectsSection.tsx
@@ -1,7 +1,7 @@
 'use client';
 
-import { motion } from 'framer-motion';
 import Image from 'next/legacy/image';
+import SectionTransition from '../SectionTransition';
 import ProjectCard from '../ProjectCard';
 import NavigationButton from '../NavigationButton';
 
@@ -35,38 +35,35 @@ function ProjectsSection({
 		staticContent.arrow_alt?.[currentLanguage] ||
 		fallbackContent.arrow_alt[currentLanguage];
 	return (
-		<motion.section
-			id='projectsSection'
-			className={styles.projectsSection}
-			initial={{ opacity: 0, y: 100 }}
-			whileInView={{ opacity: 1, y: 0 }}
-			transition={{ duration: 0.5 }}
-			viewport={{ once: true }}>
-			<h3>{headerText}</h3>
-
-			{projects.map(project => {
-				return (
-					<ProjectCard
-						key={project._id}
-						id={project._id}
-						imgSrc={project.imgSrc}
-						name={project.name}
-						role={project[currentLanguage].role}
-						stack={project.stack}
-						task={project[currentLanguage].task}
-						liveLink={project.liveLink ? project.liveLink : undefined}
-						description={project[currentLanguage].description}
-						githubLink={project.githubLink}
-					/>
-				);
-			})}
-
-			<NavigationButton text={navButtonText} targetId='aboutSection' style='underlined' />
-
-			<div className={styles.imgWrapper}>
-				<Image src={iconArrow} alt={arrowAltText} />
-			</div>
-		</motion.section>
+		<SectionTransition>
+			<section id='projectsSection' className={styles.projectsSection}>
+				<h3>{headerText}</h3>
+				{projects.map(project => {
+					return (
+						<ProjectCard
+							key={project._id}
+							id={project._id}
+							imgSrc={project.imgSrc}
+							name={project.name}
+							role={project[currentLanguage].role}
+							stack={project.stack}
+							task={project[currentLanguage].task}
+							liveLink={project.liveLink ? project.liveLink : undefined}
+							description={project[currentLanguage].description}
+							githubLink={project.githubLink}
+						/>
+					);
+				})}
+				<NavigationButton
+					text={navButtonText}
+					targetId='aboutSection'
+					style='underlined'
+				/>
+				<div className={styles.imgWrapper}>
+					<Image src={iconArrow} alt={arrowAltText} />
+				</div>
+			</section>
+		</SectionTransition>
 	);
 }
 
